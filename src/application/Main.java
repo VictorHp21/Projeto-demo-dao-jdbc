2e170080e5e5435525664ddcc18fc28d777c546c
Victor Henrique <102931817+VictorHp21@users.noreply.github.com>
2025-08-31 16:34:39 -0300

    Department class

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import Model.entities.Department;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+
+    }
+}
